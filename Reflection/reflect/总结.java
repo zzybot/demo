d14134764ca81aaaddfaf5638c81bd6110874e0a
zzybot <zzybot@163.com>
2020-10-10 15:11:16 +0800

    Create 总结.java

---
+++ Reflection/reflect/总结.java
@@ -0,0 +1,9 @@
+package reflect;
+
+public class 总结 {
+
+//    简单总结：1、获取class对象的三种方法
+//    2、获取Filed、Method、Constructor对象的方法（get.../getDeclared...）
+//    3、以上三种对象的一些常用方法如：获取/设置成员变量的值、运行方法、创建对象等
+//    4、忽略安全检查：setAccessble（true）
+}
